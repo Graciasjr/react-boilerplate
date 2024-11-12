2673373cc9580942537d9586dc979fab69426bbe
Personal Project <graciasdagadgc@gmail.com>
2024-11-12 14:59:03 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+     <div className='w-full h-[100vh] flex items-center justify-center gap-2 bg-blue-200'>
+        Welcome to React boilerplate
+     </div>
+    </>
+  )
+}
+
+export default App
